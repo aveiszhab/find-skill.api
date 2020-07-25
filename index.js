954b1e6447e17eaf089a7118e5d2a0ef057b77fa
Aniko Veiszhab <veiszhab.aniko@gmail.com>
2020-07-25 20:12:45 +0100

    Mongodb connection moved to config/db.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 /* eslint-disable no-console */
 const app = require('./src/app');
-const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+
+dotenv.config({ path: './config/.env' });
+
+connectDB();
 
 const APP_PORT = process.env.PORT || 3000;
 
-const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-};
-
-mongoose.connect(process.env.DB_CONNECTION, options, () => {
-  app.listen(APP_PORT, () => {
-    console.log(`Now serving your Express app at http://localhost:${APP_PORT}`);
-  });
+app.listen(APP_PORT, () => {
+  console.log(`Now serving your Express app at http://localhost:${APP_PORT}`);
 });
