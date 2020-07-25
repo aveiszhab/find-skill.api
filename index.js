ff69d31bf071780cf9582f0d728d95376a73c938
Aniko Veiszhab <veiszhab.aniko@gmail.com>
2020-07-25 19:18:02 +0100

    Atlas MongoDB cluster connected

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+/* eslint-disable no-console */
 const app = require('./src/app');
+const mongoose = require('mongoose');
 
 const APP_PORT = process.env.PORT || 3000;
 
-app.listen(APP_PORT, () => {
-  console.log(`Now serving your Express app at http://localhost:${APP_PORT}`); // eslint-disable-line
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+};
+
+mongoose.connect(process.env.DB_CONNECTION, options, () => {
+  app.listen(APP_PORT, () => {
+    console.log(`Now serving your Express app at http://localhost:${APP_PORT}`);
+  });
 });
